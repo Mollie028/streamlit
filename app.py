64163f2fb529e5930e6beb52830d82762c1f995e
Mollie028 <y840530@gmail.com>
2025-06-17 14:12:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ import streamlit as st
 # ❶ 全站只呼叫一次，最一開始就設
 st.set_page_config(page_title="名片辨識系統", layout="centered")
 
-# ❷ import 各頁的 render() 函數
-from frontend.pages import (
-    login,
-    首頁,
-    名片拍照,
-    語音備註,
-    查詢名片紀錄,
-    帳號管理,
-    資料匯出,
-    名片刪除,
-)
 
 # ❸ 初始化頁面指標
 if "page" not in st.session_state:
