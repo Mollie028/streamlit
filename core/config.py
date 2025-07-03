14b90889e338021b40582ba26e278516ac2e55aa
Mollie028 <y840530@gmail.com>
2025-07-03 16:27:05 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,1 @@
-import os
-
-API_BASE = os.getenv("API_BASE", "https://ocr-whisper-production-2.up.railway.app")
-
-DB_URL = os.getenv("DATABASE_URL")
-print("🧪 Streamlit 前端抓到的 DB_URL 是：", DB_URL)
-print("🧪 Streamlit 前端抓到的 API_BASE 是：", API_BASE)
+API_BASE = "https://ocr-whisper-production-2.up.railway.app"
