cb3095e125894e24bf0f866f1536391838aa2188
Mollie028 <y840530@gmail.com>
2025-07-07 09:53:49 +0800

    Update auth_service.py

--- services/auth_service.py
+++ services/auth_service.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import requests
 import psycopg2
-from core.config import API_BASE, DB_URL
+from core.config import API_BASE
 
 
 def create_user(username, password, role="user", company_name=None):
